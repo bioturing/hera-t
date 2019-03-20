5dbcb51318fe85933ac1e2b34d6dde85fa0c6aa9
Thang Tran <thang.tq.139@gmail.com>
2019-03-20 14:04:02 +0700

    need test

--- src/atomic.h
+++ src/atomic.h
@@ -3,14 +3,19 @@
 
 #if  defined(_WIN32) || defined(_WIN64)
 #include <Windows.h>
+#include <intrin.h>
 
-#define __sync_fetch_and_add32 InterlockedExchangeAdd
-#define __sync_val_compare_and_swap32(ptr, a, v) InterlockedCompareExchange(ptr, v, a)
-#define __sync_bool_compare_and_swap32(ptr, a, v) (InterlockedCompareExchange(ptr, v, a) == (a))
+#define __sync_fetch_and_add32 _InterlockedAdd
+#define __sync_val_compare_and_swap32(ptr, a, v) _InterlockedCompareExchange(ptr, v, a)
+#define __sync_bool_compare_and_swap32(ptr, a, v) (_InterlockedCompareExchange(ptr, v, a) == (a))
 
-#define __sync_fetch_and_add64 InterlockedExchangeAdd64
-#define __sync_val_compare_and_swap64(ptr, a, v) InterlockedCompareExchange64(ptr, v, a)
-#define __sync_bool_compare_and_swap64(ptr, a, v) (InterlockedCompareExchange64(ptr, v, a) == (a))
+#define __sync_fetch_and_add64 _InterlockedAdd64
+#define __sync_val_compare_and_swap64(ptr, a, v) _InterlockedCompareExchange64(ptr, v, a)
+#define __sync_bool_compare_and_swap64(ptr, a, v) (_InterlockedCompareExchange64(ptr, v, a) == (a))
+
+#define __sync_fetch_and_add8 _InterlockedAdd8
+#define __sync_val_compare_and_swap8(ptr, a, v) _InterlockedCompareExchange8(ptr, v, a)
+#define __sync_bool_compare_and_swap8(ptr, a, v) (_InterlockedCompareExchange8(ptr, v, a) == (a))
 
 #else
 #define __sync_fetch_and_add8 __sync_fetch_and_add
